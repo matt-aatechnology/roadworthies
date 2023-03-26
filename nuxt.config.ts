56067779b1ae90f75c8b13942a360316b0a0bc3a
matt-aatechnology <matt@aatechnology.com.au>
2023-03-26 13:12:59 +1100

    project files added

---
+++ nuxt.config.ts
@@ -0,0 +1,53 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+
+
+    app: {
+        head: {
+          meta: [
+            // <meta name="viewport" content="width=device-width, initial-scale=1">
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+          ],
+          link:[
+            {rel:'icon', type:'image/x-icon', href:'favicon.png'}
+          ],
+          script: [
+            {src: 'js/vendor/jquary-3.6.0.min.js', body:true},
+            {src:'js/vendor/modernizr-2.8.3-respond-1.4.2.min.js', body:true},
+            {src:'js/vendor/jquery.ajaxchimp.min.js', body:true},
+            {src:'js/vendor/bootstrap.min.js', body:true},
+            {src:'js/vendor/popper.min.js', body:true},
+            // {src:'js/vendor/swiper.min.js', body:true},
+            {src:'js/vendor/jquery.nice-select.min.js', body:true},
+            // {src:'js/vendor/venobox.min.js', body:true},
+            // {src:'js/vendor/wow.min.js', body:true},
+            {src:'js/main.js', body:true},
+
+          ]
+        }
+      },
+
+
+css:[
+'assets/css/bootstrap.min.css',
+'assets/css/animate.min.css',
+'assets/css/fontawesome.min.css',
+'assets/css/line-awesome.min.css',
+'assets/css/keyframe-animation.css',
+'assets/css/nice-select.css',
+// 'assets/css/venobox.min.css',
+// 'assets/css/swiper.min.css',
+'assets/css/elements.css',
+'assets/css/header.css',
+'assets/css/slider.css',
+'assets/css/blog.css',
+'assets/css/main.css',
+'assets/css/responsive.css'
+]
+
+
+
+
+})
+
+
---
+++ pages/index.vue
@@ -0,0 +1,425 @@
+
+<template>
+
+
+    <div class="site-preloader">
+        <div class="car">
+            <div class="strike"></div>
+            <div class="strike strike2"></div>
+            <div class="strike strike3"></div>
+            <div class="strike strike4"></div>
+            <div class="strike strike5"></div>
+            <div class="car-detail spoiler"></div>
+            <div class="car-detail back"></div>
+            <div class="car-detail center"></div>
+            <div class="car-detail center1"></div>
+            <div class="car-detail front"></div>
+            <div class="car-detail wheel"></div>
+            <div class="car-detail wheel wheel2"></div>
+        </div>
+    </div>
+
+    <header class="main-header">
+
+        <div class="top-header">
+            <div class="container">
+                <div class="top-header-wrap">
+                    <div class="top-left">
+                        <p>RWC Now Mon - Sat 7AM to 6PM</p>
+                    </div>
+                    <div class="top-right">
+                        <ul class="top-header-nav">
+                            <li><a href="faqs.html">Help</a></li>
+                            <li><a href="contact.html">Support</a></li>
+                            <li><a href="faqs.html">FAQ</a></li>
+                        </ul>
+                        <ul class="header-social-share">
+                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+        <div class="mid-header">
+            <div class="container">
+                <div class="mid-header-wrap">
+                    <div class="site-logo">
+                        <a href="index.html"><img src="~/assets/img/logo-dark.png" alt="Logo"></a>
+                    </div>
+                    <ul class="header-info">
+                        <li>
+                            <div class="header-info-icon">
+                                <i class="las la-phone-volume"></i>
+                            </div>
+                            <div class="header-info-text">
+                                <h3><span>Call Now:</span><a href="tel:0401813031">0401 813 031</a></h3>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="header-info-icon">
+                                <i class="las la-map-marked-alt"></i>
+                            </div>
+                            <div class="header-info-text">
+                                <h3><span>Mobile RWC Service</span>We Come To You</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="header-info-icon">
+                                <i class="las la-map-marked-alt"></i>
+                            </div>
+                            <div class="header-info-text">
+                                <h3><span>RWC Safety Certificates</span>Get RWC Faster</h3>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+        <div class="nav-menu-wrapper">
+            <div class="container">
+                <div class="nav-menu-inner">
+                    <div class="site-logo">
+                        <a href="index.html"><img src="~/assets/img/logo-dark.png" alt="Logo"></a>
+                    </div>
+                    <div class="header-menu-wrap">
+                        <ul class="nav-menu">
+                            <li class="active dropdown_menu">
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li class="dropdown_menu">
+                                <a href="about-us.html">RoadWorthy Safety Certificates</a>
+                                <ul>
+                                    <li><a href="about-us.html">Cars</a></li>
+                                    <li><a href="about-company.html">Bikes & Motorcycles</a></li>
+                                    <li><a href="our-services.html">Trailers</a></li>
+                                    <li><a href="service-details.html">Caravans</a></li>
+                                    <li><a href="book-taxi.html">Motorhomes</a></li>
+                                    <li><a href="book-taxi.html">Light Trucks</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown_menu">
+                                <a href="our-taxi.html">Service Areas</a>
+                                <ul>
+                                    <li><a href="our-taxi.html">Brisbane</a></li>
+                                    <li><a href="taxi-details.html">Gold Coast</a></li>
+                                    <li><a href="taxi-details.html">Sunshine Coast</a></li>
+                                </ul>
+                            </li>
+
+                            <li><a href="contact.html">Fast Booking Enquiry</a></li>
+                            <li><a href="contact.html">Pricing</a></li>
+                            <li class="dropdown_menu">
+                                <a href="blog-grid.html">FAQ</a>
+                                <ul>
+                                    <li><a href="blog-grid.html">Grid Posts</a></li>
+                                    <li><a href="blog-classic.html">Classic Posts</a></li>
+                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="menu-right-item">
+
+                        <a href="book-taxi.html" class="menu-btn">BOOK NOW!</a>
+
+                    </div>
+                    <div class="mobile-menu-icon">
+                        <div class="burger-menu">
+                            <div class="line-menu line-half first-line"></div>
+                            <div class="line-menu"></div>
+                            <div class="line-menu line-half last-line"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+   
+    </header>
+   
+
+
+
+
+    
+
+    <div class="slider-section">
+        <div class="main-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+
+                    <div class="slider-img"><img src="~/assets/images/home/cars-1.jpg" alt="img" class="kenburns"
+                            style="animation-duration: 12000ms; ">
+                    </div>
+
+
+                    <div class="car-holder">
+                        <!-- <img src="assets/images/rwc-inspectors.png" alt="rwc inspectors people" data-animation="car-animation" data-duration="1.5s"> -->
+                        <div class="shape">
+                            <div data-animation="slide-in-bottom" data-duration="1.5s" data-delay="0.3s"></div>
+                            <div data-animation="fade-in" data-duration="1.5s" data-delay="0.2s"></div>
+                            <div data-animation="slide-in-top" data-duration="1.5s" data-delay="0.5s"></div>
+                        </div>
+                    </div>
+
+                    <div class="slider-content-wrap d-flex align-items-center text-left">
+                        <div class="container">
+                            <div class="slider-content">    
+                                <div class="slider-caption medium">
+                                    <div class="inner-layer">
+                                        <div data-animation="fade-in-bottom" data-delay="0.3s">GET YOUR RWC FASTER!
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="slider-caption big">
+                                    <div class="inner-layer">
+                                        <div data-animation="fade-in-bottom" data-delay="0.5s">
+                                            Mobile Roadworthy Certificates
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="slider-caption small">
+                                    <div class="inner-layer">
+                                        <div data-animation="fade-in-bottom" data-delay="0.7s" data-duration="1s"
+                                        style="background-color: #333333; padding:4px 4px 4px 16px;">
+                                        Brisbane | Sunshine Coast | Gold Coast
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="slider-btn">
+                                    <a href="book-taxi.html" class="default-btn" data-animation="fade-in-bottom"
+                                        data-delay="0.9s">Book Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+               
+            </div>
+        </div>
+    </div>
+ 
+
+
+
+    <section class="about-section padding">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <div class="about-img">
+                        <img class="about-img1 wow fade-in-left" data-wow-delay="200ms" src="~/assets/images/home/girl-new-car.jpg" alt="girl with new car">
+                        <img class="about-img2 wow fade-in-bottom" data-wow-delay="400ms" src="~/assets/images/home/tyre-check.jpg" alt="inspecting a tyre for rwc">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="section-heading mb-40 wow fade-in-right" data-wow-delay="200ms">
+                        <h4><span></span>GET YOUR RWC FASTER</h4>
+                        <h2>Mobile RWC Safety Certificates</h2>
+                        <p>A roadworthy certificate, safety certificate or RWC is required when buying, selling or transferring 
+                            a vehicle. &nbsp; Our certified inspectors come to you - providing a fast, efficient and convenient service
+                            right to your door.  Our mobile safety certificates include cars, bikes, trailers, caravans, motorhomes and light trucks. &nbsp; Book your safety certificate today!
+                        </p>
+                    </div>
+                
+                    <ul class="about-info wow fade-in-right" data-wow-delay="400ms">
+                        <li>
+                            <img class="owner-thumb" src="~/assets/images/home/tick.png" alt="tick of approval">
+                            <div class="owner">
+                                <h3>John </h3><br>
+                                <span style="color:gray">Roadworthies</span>
+                              
+                            </div>
+                        </li>
+                        <li>
+                            <h2><span>CALL US NOW</span><a href="tel:0401813031">0401 813 031</a></h2>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+
+
+
+    
+
+
+
+
+
+
+
+    <section class="download-section bd-bottom padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="section-heading wow fade-in-left" data-wow-delay="200ms">
+                        <h4><span></span>RWC FOR A RANGE OF VEHICLES</h4>
+                        <h2>What does a mobile RWC safety certificate cover?</h2>
+                        <p>Our mobile roadworthy inspection looks at the condition and functionality of the vehicle including the inspection of brakes, tyres, suspension, windscreen, lights, seatbelts and more. &nbsp; Our friendly RWC inspectors here to guide you through the process and get a RWC in your hand asap! </p>
+                        <ul class="app-feature">
+                            <h3>We provide mobile safety certificates for:</h3>
+                            <li>Cars</li>
+                            <li>Bikes</li>
+                            <li>Trailers</li>
+                            <li>Caravans</li>
+                            <li>Motorhomes</li>
+                            <li>Light Trucks</li>
+                        </ul>
+
+                        <ul class="app-feature">
+                            <h3>Our service areas include:</h3>
+                            <li>Brisbane</li>
+                            <li>Gold Coast</li>
+                            <li>Sunshine Coast</li>
+                        </ul>
+   
+                    </div>
+                </div>
+                <div class="col-lg-6 d-none d-lg-block">
+                    <div class="taxi-holder">
+                        <div class="taxi wow fade-in-left" data-wow-delay="300ms"></div>
+                        <div class="mockup wow fade-in-bottom" data-wow-delay="400ms"></div>
+                        <div class="charecter wow fade-in-right" data-wow-delay="500ms"></div>
+                        <img class="location-marker" src="~/assets/img/location-marker.png" alt="location">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+   
+
+
+
+
+    
+
+
+    <footer class="footer-section">
+        <div class="footer-top-wrap bg-grey">
+            <div class="container">
+                <div class="footer-top">
+                    <div class="row align-items-end">
+                        <div class="col-lg-8">
+                            <div class="brand">
+                                <a class="footer-logo" href="index.html"><img src="~/assets/img/logo-light.png" alt="logo"></a>
+                                <p>Mobile roadworthy RWC safety certificates for cars, bikes, trailers, caravans, motorhomes, light trucks </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="footer-call">
+                                <i class="las la-phone-volume"></i>
+                                <p><span>Call Now</span><a href="tel:0401813031">0401 813 031</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+
+        <div class="footer-mid-wrap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item">
+                            <div class="widget-title">
+                                <h3>Working Hours</h3>
+                            </div>
+                            <ul class="footer-contact">
+                                <li><span>Monday - Friday:</span>9.00am To 8.00pm</li>
+                                <li><span>Saturday:</span>10.00am To 7.30pm</li>
+                                <li><span>Sunday:</span>Close Day!</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item footer-list">
+                            <div class="widget-title">
+                                <h3>Usefull Links</h3>
+                            </div>
+                            <ul class="footer-links">
+                                <li><a href="book-taxi.html">Taxi Booking</a></li>
+                                <li><a href="faqs.html">Help Center</a></li>
+                                <li><a href="about-us.html">Privacy and Policy</a></li>
+                                <li><a href="about-company.html">Terms of Use</a></li>
+                                <li><a href="contact.html">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item">
+                            <div class="widget-title">
+                                <h3>Head Office</h3>
+                            </div>
+                            <ul class="footer-contact">
+                                <li><span>Location:</span>153 Williamson Plaza, Maggieberg, MT 09514</li>
+                                <li><span>Join Us:</span>Info@YourGmail24.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item subscribe-wrap">
+                            <div class="widget-title">
+                                <h3>Newsletter Signup</h3>
+                            </div>
+                            <form action="#" class="subscribe-form">
+                                <input class="form-control" type="email" name="email" placeholder="Your Email" >
+                                <input type="hidden" name="action" value="mailchimpsubscribe">
+                                <button class="submit">Subscribe Now</button>
+                                <div class="clearfix"></div>
+                                <div id="subscribe-result">
+                                    <div class="subscription-success"></div>
+                                    <div class="subscription-error"></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="running-taxi">
+                <div class="taxi"></div>
+                <div class="taxi-2"></div>
+                <div class="taxi-3"></div>
+            </div>
+        </div>
+
+
+        <div class="copyright-wrap">
+            <div class="container">
+                <p>website by <a href="https://aatechnology.com.au" style="color:yellow">All About Technology</a></p>
+            </div>
+        </div>
+ 
+    </footer>
+ 
+ 
+
+
+
+    <div class="dl-cursor">
+        <div class="cursor-icon-holder"><i class="las la-times"></i></div>
+    </div>
+
+
+  </template>
+
